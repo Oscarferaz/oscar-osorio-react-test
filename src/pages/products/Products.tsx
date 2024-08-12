e05c6f8eb1094185923bd8d8251f68c1ba72f7d9
Oscarferaz <o.osorio@feraz.com.mx>
2024-08-11 23:11:06 -0600

    Refactorizacion Navbar

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,13 +1,10 @@
-import { useNavigate } from "react-router-dom"
 import { TableProducts, Navbar, DropDownHeader } from "./components"
 
 const Products = () => {
-    const navigate = useNavigate()
 
     return(
         <>
             <Navbar>
-                {/* <button onClick={() => navigate('/users')}>usuarios</button> */}
                 <DropDownHeader/>
             </Navbar>
             <div className="container-flex">
